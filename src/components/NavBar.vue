0c26ffce922d9e0567b1261ae31bf56b748bab80
rafaela <you@example.com>
2023-04-12 19:17:04 -0300

    Reescrevendo o vue router e possibilitando a navegação entre páginas, tudo que está em APP é fixo e tudo q esta na router muda de router para router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">
+        <img src="" id="logo" alt="logo">
+      </router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
